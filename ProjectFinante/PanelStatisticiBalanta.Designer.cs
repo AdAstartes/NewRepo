540e21bc018c1a7882ed205975691db1db40b9a7
Emil Antoni <emil.antoniandras@gmail.com>
2021-07-01 20:37:42 +0300

    sdfdsfdsf

---
+++ ProjectFinante/PanelStatisticiBalanta.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace ProjectFinante
+{
+    partial class PanelStatisticiBalanta
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label_Panala = new System.Windows.Forms.Label();
+            this.label_Dela = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(82, 273);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(276, 31);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Exporta datele";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(82, 184);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(276, 31);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Calculeaza valoarea totala";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(82, 228);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(276, 31);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Calculeaza valoarea medie";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Venituri fixe vs Cheltuieli fixe",
+            "Venituri recurente vs Cheltuieli recurente",
+            "Venituri vs Cheltuieli ( per total )"});
+            this.comboBox1.Location = new System.Drawing.Point(82, 143);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(276, 23);
+            this.comboBox1.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(183, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 15);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Tip Balanta";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(268, 56);
+            this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(132, 23);
+            this.dateTimePicker2.TabIndex = 12;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(34, 56);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(132, 23);
+            this.dateTimePicker1.TabIndex = 11;
+            // 
+            // label_Panala
+            // 
+            this.label_Panala.AutoSize = true;
+            this.label_Panala.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label_Panala.Location = new System.Drawing.Point(283, 29);
+            this.label_Panala.Name = "label_Panala";
+            this.label_Panala.Size = new System.Drawing.Size(89, 15);
+            this.label_Panala.TabIndex = 10;
+            this.label_Panala.Text = "Pana la data de";
+            // 
+            // label_Dela
+            // 
+            this.label_Dela.AutoSize = true;
+            this.label_Dela.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label_Dela.Location = new System.Drawing.Point(53, 29);
+            this.label_Dela.Name = "label_Dela";
+            this.label_Dela.Size = new System.Drawing.Size(79, 15);
+            this.label_Dela.TabIndex = 9;
+            this.label_Dela.Text = "De la data de";
+            // 
+            // PanelStatisticiBalanta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Beige;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label_Panala);
+            this.Controls.Add(this.label_Dela);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "PanelStatisticiBalanta";
+            this.Size = new System.Drawing.Size(432, 370);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label_Panala;
+        private System.Windows.Forms.Label label_Dela;
+    }
+}
---
+++ ProjectFinante/PanelStatisticiBalanta.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectFinante
+{
+    public partial class PanelStatisticiBalanta : UserControl
+    {
+        public PanelStatisticiBalanta()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DateTime date1 = dateTimePicker1.Value;
+
+            DateTime date2 = dateTimePicker2.Value;
+            int recurenta = comboBox1.SelectedIndex;
+            Dictionary<string, float> returndict;
+            switch (recurenta)
+            { 
+                case 0:
+                    MessageBox.Show((Money.calculate_total("Fixe", date1, date2, "venituri")["value"] - Money.calculate_total("Fixe", date1, date2, "cheltuieli")["value"]).ToString());
+                    break;
+                case 1:
+                    MessageBox.Show((Money.calculate_total("Recurente", date1, date2, "venituri")["value"] - Money.calculate_total("Recurente", date1, date2, "cheltuieli")["value"]).ToString());
+                    break;
+                case 2:
+                    MessageBox.Show((Money.calculate_total("Cookies", date1, date2, "venituri")["value"] - Money.calculate_total("asdasdas", date1, date2, "cheltuieli")["value"]).ToString());
+                    break;
+                default:
+                    MessageBox.Show((Money.calculate_total("Tasty", date1, date2, "venituri")["value"] - Money.calculate_total("adfgagsa", date1, date2, "cheltuieli")["value"]).ToString());
+                    break;
+            }
+    
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime date1 = dateTimePicker1.Value;
+
+            DateTime date2 = dateTimePicker2.Value;
+            int recurenta = comboBox1.SelectedIndex;
+            Dictionary<string, float> returndict;
+            switch (recurenta)
+            {
+                case 0:
+                    {
+                        Dictionary<string, float> venituri=Money.calculate_total("Fixe", date1, date2, "venituri");
+                        Dictionary<string, float> cheltuieli = Money.calculate_total("Fixe", date1, date2, "cheltuieli");
+                        MessageBox.Show(((venituri["value"]-cheltuieli["value"])/(venituri["count"]+cheltuieli["count"])).ToString());
+                        break;
+                    }
+                case 1:
+                    {
+                        Dictionary<string, float> venituri = Money.calculate_total("Recurente", date1, date2, "venituri");
+                        Dictionary<string, float> cheltuieli = Money.calculate_total("Recurente", date1, date2, "cheltuieli");
+                        MessageBox.Show(((venituri["value"] - cheltuieli["value"]) / (venituri["count"] + cheltuieli["count"])).ToString());
+                        break;
+                    }
+                case 2:
+                    {
+                        Dictionary<string, float> venituri = Money.calculate_total("123", date1, date2, "venituri");
+                        Dictionary<string, float> cheltuieli = Money.calculate_total("Recadsdaurente", date1, date2, "cheltuieli");
+                        MessageBox.Show(((venituri["value"] - cheltuieli["value"]) / (venituri["count"] + cheltuieli["count"])).ToString());
+                        break;
+                    }
+                    break;
+                default:
+                    {
+                        Dictionary<string, float> venituri = Money.calculate_total("123", date1, date2, "venituri");
+                        Dictionary<string, float> cheltuieli = Money.calculate_total("Recadsdaurente", date1, date2, "cheltuieli");
+                        MessageBox.Show(((venituri["value"] - cheltuieli["value"]) / (venituri["count"] + cheltuieli["count"])).ToString());
+                        break;
+                    }
+               
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
